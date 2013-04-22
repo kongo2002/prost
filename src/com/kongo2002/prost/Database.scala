889f8cd3fdd1ee44814c830daeefccd48e593444
kongo2002 <kongo2002@googlemail.com>
2013-04-22 22:56:10 +0200

    fix indentation

--- src/com/kongo2002/prost/Database.scala
+++ src/com/kongo2002/prost/Database.scala
@@ -7,68 +7,72 @@ import android.database.SQLException
 import android.util.Log
 
 object DrinksDatabase {
-  
+
   private final def DBVERSION = 1
   private final def DBNAME = "drinks"
-  
+
   class DrinksOpenHelper(context: Context) extends ProstOpenHelper(context, DBNAME, DBVERSION) {
-    
+
     final def CREATE_TABLE = "CREATE TABLE " + DBNAME + " (" +
       "_id INTEGER PRIMARY KEY AUTOINCREMENT, drink INTEGER, date INTEGER)"
+
   }
 }
 
 object DrinkTypesDatabase {
-  
+
   private final def DBVERSION = 1
   private final def DBNAME = "drink_types"
-    
+
   class DrinkTypesDatabase(context: Context) extends ProstOpenHelper(context, DBNAME, DBVERSION) {
-    
+
    final def CREATE_TABLE = "CREATE TABLE " + DBNAME + " (" +
      "_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, unit INTEGER)"
+
   }
 }
 
 abstract class ProstOpenHelper(context: Context, db: String, version: Int)
   extends SQLiteOpenHelper(context, db, null, version) {
-    
-    final def DBNAME = db
-    final def DBVERSION = version
-  
-    protected final def DROP_TABLE = "DROP TABLE IF EXISTS " + DBNAME
-    
-    def CREATE_TABLE : String
-    
-    override def onCreate(db: SQLiteDatabase) {
-      Log.w(Prost.LOG_TAG, "Creating database '" + DBNAME + "'")
-      
-      db.beginTransaction()
-      
-      try {
-        db.execSQL(CREATE_TABLE)
-        db.setTransactionSuccessful()
-      } catch {
-        case sex: SQLException => Log.e("Error creating database '" + DBNAME + "'", sex.toString())
-      } finally {
+
+  final def DBNAME = db
+  final def DBVERSION = version
+
+  protected final def DROP_TABLE = "DROP TABLE IF EXISTS " + DBNAME
+
+  def CREATE_TABLE : String
+
+  override def onCreate(db: SQLiteDatabase) {
+    Log.w(Prost.LOG_TAG, "Creating database '" + DBNAME + "'")
+
+    db.beginTransaction()
+
+    try {
+      db.execSQL(CREATE_TABLE)
+      db.setTransactionSuccessful()
+    } catch {
+      case sex: SQLException => Log.e("Error creating database '" + DBNAME + "'", sex.toString())
+    } finally {
         db.endTransaction()
-      }
-    }
-  
-    override def onUpgrade(db: SQLiteDatabase, oldV: Int, newV: Int) {
-      Log.w(Prost.LOG_TAG, "Upgrading database '" + DBNAME + "' from version " + oldV +
-        " to " + newV + ", which will currently destroy all existing data!")
-      
-      db.beginTransaction()
-    
-      try {
-        db.execSQL(DROP_TABLE)
-        db.execSQL(CREATE_TABLE)
-        db.setTransactionSuccessful()
-      } catch {
-        case sex: SQLException => Log.e("Error updating database '" + DBNAME + "'", sex.toString())
-      } finally {
-        db.endTransaction()
-      }
     }
   }
+
+  override def onUpgrade(db: SQLiteDatabase, oldV: Int, newV: Int) {
+    Log.w(Prost.LOG_TAG, "Upgrading database '" + DBNAME + "' from version " + oldV +
+      " to " + newV + ", which will currently destroy all existing data!")
+
+    db.beginTransaction()
+
+    try {
+      db.execSQL(DROP_TABLE)
+      db.execSQL(CREATE_TABLE)
+      db.setTransactionSuccessful()
+    } catch {
+      case sex: SQLException => Log.e("Error updating database '" + DBNAME + "'", sex.toString())
+    } finally {
+        db.endTransaction()
+    }
+  }
+}
+
+/* vim: set et sw=2 sts=2: */
--- src/com/kongo2002/prost/Prost.scala
+++ src/com/kongo2002/prost/Prost.scala
@@ -3,3 +3,5 @@ package com.kongo2002.prost
 object Prost {
   final def LOG_TAG = "prost"
 }
+
+/* vim: set et sw=2 sts=2: */
