87dafdb53cd571750aad0fc5efdc7bf9f213a995
kongo2002 <kongo2002@googlemail.com>
2013-05-02 21:52:36 +0200

    add linear layouts to typed resources

--- src/com/kongo2002/prost/TypedResource.scala
+++ src/com/kongo2002/prost/TypedResource.scala
@@ -4,12 +4,21 @@ import android.app.Activity
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 
 case class TypedResource[T](id : Int)
 
 object TR {
   val newBeerBtn = TypedResource[Button](R.id.new_beer)
+  val topLeftTile = TypedResource[LinearLayout](R.id.tl_layout)
+  val topTile = TypedResource[LinearLayout](R.id.t_layout)
+  val topRightTile = TypedResource[LinearLayout](R.id.tr_layout)
+  val leftTile = TypedResource[LinearLayout](R.id.l_layout)
+  val rightTile = TypedResource[LinearLayout](R.id.r_layout)
+  val bottomLeftTile = TypedResource[LinearLayout](R.id.bl_layout)
+  val bottomTile = TypedResource[LinearLayout](R.id.b_layout)
+  val bottomRightTile = TypedResource[LinearLayout](R.id.br_layout)
 }
 
 trait TypedViewHolder {
