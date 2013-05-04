2d404981ce92aa8ec7e722126cab7be7e1a54b1c
kongo2002 <kongo2002@googlemail.com>
2013-05-05 00:16:05 +0200

    better use Editor.apply()
    NOTE: the parenthesis are crucial here!

--- src/com/kongo2002/prost/MainActivity.scala
+++ src/com/kongo2002/prost/MainActivity.scala
@@ -230,7 +230,7 @@ class MainActivity extends TypedActivity
 
     /* put and commit changes */
     editor.putString(key, cmd)
-    editor.commit
+    editor.apply()
   }
 
   private def addClickHandlers {
