325450a26141b56d879e93535f15687d317887b3
kongo2002 <kongo2002@googlemail.com>
2013-07-10 00:53:37 +0200

    add options menu to drinks fragment

--- src/com/kongo2002/android/prost/DrinksFragment.scala
+++ src/com/kongo2002/android/prost/DrinksFragment.scala
@@ -4,14 +4,26 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.MenuInflater
+import android.view.Menu
 
 class DrinksFragment extends Fragment
   with Loggable {
 
   override def onCreateView(inf: LayoutInflater, c: ViewGroup, b: Bundle) = {
     logI("onCreateView")
+
     val view = inf.inflate(R.layout.drinks_fragment, c, false)
+
+    setHasOptionsMenu(true)
+
     view
+  }
+
+  override def onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+    inflater.inflate(R.menu.drinks, menu)
+
+    super.onCreateOptionsMenu(menu, inflater)
   }
 
 }
