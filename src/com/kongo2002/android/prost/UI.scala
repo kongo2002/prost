18fad696f7251935927b1813ab566ed54f25c6ea
kongo2002 <kongo2002@googlemail.com>
2013-07-24 23:56:31 +0200

    extend UI utility functions

--- src/com/kongo2002/android/prost/UI.scala
+++ src/com/kongo2002/android/prost/UI.scala
@@ -16,12 +16,15 @@
 
 package com.kongo2002.android.prost
 
+import android.app.Activity
+import android.app.ActivityManager
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
-import android.widget.Toast
-import Implicits._
 import android.database.Cursor
+import android.widget.Toast
+
+import Implicits._
 
 
 object UI {
@@ -29,19 +32,23 @@ object UI {
   /**
    * Create and show a Toast for a specified period of time.
    */
-  def toast(duration: Int)(ctx: Context, msg: String) {
+  def toast(ctx: Context, msg: String, duration: Int) {
     Toast.makeText(ctx, msg, duration).show()
   }
 
   /**
    * Create and show a Toast for a long period of time.
    */
-  def longToast(ctx: Context, msg: String) = toast(Toast.LENGTH_LONG) _
+  def longToast(ctx: Context, msg: String) {
+    toast(ctx, msg, Toast.LENGTH_LONG)
+  }
 
   /**
    * Create and show a Toast for a short period of time.
    */
-  def shortToast(ctx: Context, msg: String) = toast(Toast.LENGTH_SHORT) _
+  def shortToast(ctx: Context, msg: String) {
+    toast(ctx, msg, Toast.LENGTH_SHORT)
+  }
 
   /**
    * Create and show a confirmation dialog and hook into
@@ -95,4 +102,15 @@ object UI {
     dialog.show
   }
 
+  /**
+   * Determine whether the given activity supports
+   * at least OpenGL ES 2.0
+   * @param activity  Activity to check the OpenGL support for
+   */
+  def supportsOpenGlES2(activity: Activity) = {
+    val actManager = activity.getSystemService(Context.ACTIVITY_SERVICE).asInstanceOf[ActivityManager]
+    val configInfo = actManager.getDeviceConfigurationInfo
+
+    configInfo.reqGlEsVersion >= 0x20000
+  }
 }
